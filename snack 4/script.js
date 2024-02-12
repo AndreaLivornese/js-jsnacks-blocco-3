c07abd94e82fe539ce9f18e8de7f907885e61580
andrea-livornese <andrealivornese@gmail.com>
2024-02-12 17:31:19 +0100

    creazione della struttura dello script

---
+++ snack 4/script.js
@@ -0,0 +1,40 @@
+// Scrivi una funzione  che accetti una stringa come parametro e restituisca la stringa con i caratteri ordinati alfabeticamente
+// Es: riordina("webmaster") :freccia_destra: "abeemrstw"
+
+
+
+
+const array = ['c','i,','c','o','g','n','a'];
+
+
+console.log(stringSort(array));
+
+
+
+
+// funzione che ordina i caratteri all'interno di una stringaù
+
+function stringSort(str){
+
+    // for più "lento" e scorre la stringa
+    for(let i=0; i < str.length; i++){
+
+        // for più "veloce" e scorre la striga
+        for(let j=0; j< str.length; j++){
+
+            if(str[j] > str[i]){
+                // variabile di lavoro per compiere lo scambio
+                let swap = str[i];
+                str[i] = str[j];
+                str[j] = swap;
+                console.log(str);
+            }
+
+        }
+    }
+
+
+
+    return str;
+
+}
